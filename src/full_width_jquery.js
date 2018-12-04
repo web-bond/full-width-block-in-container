04483bf9e82f422940d41ce67d47b99d17b63933
Vladyslav <45554407+web-bond@users.noreply.github.com>
2018-12-04 10:29:13 +0200

    Create full_width_jquery.js

---
+++ src/full_width_jquery.js
@@ -0,0 +1,20 @@
+function fullWidthFinal(selector_container, selector_block) {
+    var selector_container = selector_container;
+    var selector_block = selector_block;
+    if ($(selector_container).length && $(selector_block).length) {
+        fullWidth(selector_container, selector_block);
+        $(window).on('resize', function() {
+            fullWidth(selector_container, selector_block);
+        });
+    }
+}
+function fullWidth(selector_container, selector_block) {
+    var selector_container = selector_container;
+    var class_block = selector_block;
+    if ($(selector_container).length && $(selector_block).length) {
+        var width = document.documentElement.clientWidth;
+        var containerWidth = $(selector_container).width();
+        var margin = -(width - containerWidth)/2;
+        $(class_block).css({'margin-left':+margin+'px','margin-right':+margin+'px'});
+    }
+}
